7474610dadfa996e2f920da2e13aa8193f3cb151
pgayathri511 <pgayathri511@gmail.com>
2026-08-13 09:54:40 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! My Python app is deployed successfully on Azure."
+
+if __name__ == "__main__":
+    app.run()
