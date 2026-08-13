f0719e78702b2f4fce359d5b991d485b83ada54f
pgayathri511 <pgayathri511@gmail.com>
2026-08-13 10:52:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask
-
-app = Flask(__name__)
-
+from  flask import Flask
+app=Flask(__name)
 @app.route("/")
 def home():
-    return "Hello! My Python app is deployed successfully on Azure."
-if __name__ == "__main__":
-    app.run()
+  return"Hello!My python app is deployed successfully on Azure."
+if __name__="__mani__":
+app.run()
